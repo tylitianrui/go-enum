39568bdcfa3f833beb9c0a67bb23991384b2cc07
tyltr <tianrui_li@qingsongchou.com>
2020-01-14 18:14:39 +0800

    inital function

---
+++ enum_test.go
@@ -0,0 +1,21 @@
+package enum
+
+import "testing"
+
+type Human struct {
+	gender int    `enum:"[1,2,3]"`
+	a      string `enum:"[li,qq]"`
+}
+
+func TestVerify(t *testing.T) {
+	h := Human{
+		4,
+		"li",
+	}
+	v := New(h)
+	if err := v.Verify(); err != nil {
+		t.Log(err)
+	} else {
+		t.Fail()
+	}
+}
